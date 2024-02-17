6b6ccc7a291bc1c4e897b01e17a9fe2036aea820
Rohan Mathew <robozmat@gmail.com>
2024-02-17 14:54:03 +0000

    detecting tags

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
